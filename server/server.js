3673319d3fd1fa032ca9076d06739e1e46c177ec
wseem85 <engwseem2@gmail.com>
2025-10-01 12:45:32 +0300

    fixing frontend url on cors

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 connectCloudinary();
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://helio-ai-nu.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://helio-ai-nu.vercel.app'],
     credentials: true,
   })
 );
